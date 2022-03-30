272b4c62ce3c27d5a33c28e194ce017eb46891f8
niteshsingh29 <niteshsingh29072000a@gmail.com>
2022-03-30 16:44:17 +0530

    sending object/json directly from the server to the client

--- expressSetup/app.js
+++ expressSetup/app.js
@@ -1,19 +1,32 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {    
-    res.send("sending from backend express")
-})
-app.get("/Home", (req, res) => {    
-    res.send("sending from Home express")
-})
-app.get("/About", (req, res) => {    
-    res.send("sending from About express")
-})
-app.get("/Contact", (req, res) => {    
-    res.send("sending from Contact express")
-})
+app.get("/", (req, res) => {
+  res.send("sending from backend express");
+});
+app.get("/Home", (req, res) => {
+  res.send("sending from Home express");
+});
+app.get("/About", (req, res) => {
+  res.send("sending from About express");
+});
+app.get("/Contact", (req, res) => {
+  res.send("sending from Contact express");
+});
 
-app.listen(8000, ()=> {
-    console.log("listening port at 8000")
-})
+app.get("/temp", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      name: "nitesh",
+    },
+    {
+      id: 1,
+      name: "nitesh",
+    },
+  ]);
+});
+
+app.listen(8000, () => {
+  console.log("listening port at 8000");
+});
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const fs = require("fs");
-const http = require("http");
-const path = require("path");
+// const fs = require("fs");
+// const http = require("http");
+// const path = require("path");
 
-console.log("welcome to backend");
+// console.log("welcome to backend");
 
-const server = http.createServer()
-server.on("request", (req, res) => {
-  fs.readFile("user.txt", "utf-8" , (err, data) => {   // reading data from file
-    if (err) return console.error(err);
-    res.end(" hello world ");
-    console.log(data);
-  })
-})
+// const server = http.createServer()
+// server.on("request", (req, res) => {
+//   fs.readFile("user.txt", "utf-8" , (err, data) => {   // reading data from file
+//     if (err) return console.error(err);
+//     res.end(" hello world ");
+//     console.log(data);
+//   })
+// })
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("listening to the server");
-});
+// server.listen(8000, "127.0.0.1", () => {
+//   console.log("listening to the server");
+// });
 
 // let obj__1 = {
 //     name: "nitesh singh",
@@ -60,3 +60,5 @@ server.listen(8000, "127.0.0.1", () => {
 // fs.unlink("j.json", (err) => {
 //     console.log("done")
 // })
+
+
